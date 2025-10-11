b816c642a30d7d7eae21f889bc4246659d5f63c7
tommyrobot666 <tommyrobot10@gmail.com>
2025-10-10 18:19:55 -0700

    inital commit

---
+++ common/src/main/java/lommie/dugdown/Constants.java
@@ -0,0 +1,11 @@
+package lommie.dugdown;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "dugdown";
+    public static final String MOD_NAME = "DugDown";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
